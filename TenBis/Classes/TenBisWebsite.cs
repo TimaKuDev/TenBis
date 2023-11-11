a5432a3ad7122a1204db6d30469deb0a634d1ca0
TimaKuDev <Timakudev@gmail.com>
2023-11-11 13:07:08 +0200

    Add project files.

---
+++ TenBis/Classes/TenBisWebsite.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Text;
+
+namespace TenBis.Classes
+{
+    internal class TenBisWebsite
+    {
+        private const string TEN_BIS_URL = "https://www.10bis.co.il/next/user-report?dateBias=0";
+        ChromeDriverService _chromeDriverService;
+        ChromeOptions _chromeOptions;
+        ChromeDriver _chromeDriver;
+        System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> _dropDownImage;
+        public TenBisWebsite()
+        {
+            _chromeDriverService = ChromeDriverService.CreateDefaultService();
+            _chromeOptions = new ChromeOptions();
+            _chromeOptions.AddArgument(@"--user-data-dir=C:\Users\User\AppData\Local\Google\Chrome\User Data\Default");
+            _chromeDriver = new ChromeDriver(_chromeDriverService, _chromeOptions);
+        }
+
+        ~TenBisWebsite()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            _chromeDriver?.Close();
+            _chromeDriver?.Quit();
+            _chromeDriver?.Dispose();
+        }
+
+        public void StartTenBisWebsite()
+        {
+            _chromeDriver.Manage().Window.Maximize();
+            _chromeDriver.Navigate().GoToUrl(TEN_BIS_URL);
+        }
+
+        public void ValidateUserLoggedIn()
+        {
+            Task.Delay(2000).Wait();
+            const string DROP_DOWN_IMAGE = @"//img[@class=""styled__Img-sc-1f9rnwm-0 bDSlBz ActionMenu__MenuTriangleButton-sc-15ye1k-1 kwtrcZ""]";
+            _dropDownImage = _chromeDriver.FindElements(By.XPath(DROP_DOWN_IMAGE));
+            if (_dropDownImage.Count == 0)
+            {
+                Task.Delay(28000);
+                ValidateUserLoggedIn();
+            }
+        }
+
+        internal void AggregateMoneyToPoints()
+        {
+            Task.Delay(1000).Wait();
+            const string DROP_DOWN_IMAGE = @"//button[@class=""Button-sc-1n9hyby-0 AddToCredit__AddCreditButton-sc-1ux4vzy-3 fQLuJl kipuFE""]";
+            System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> aggregateButton = _chromeDriver.FindElements(By.XPath(DROP_DOWN_IMAGE));
+            if (!aggregateButton[0].Enabled)
+            {
+                return;
+            }
+
+            aggregateButton[0].Click();
+           
+        }
+    }
+}
---
+++ TenBis/Program.cs
@@ -0,0 +1,27 @@
+﻿using TenBis.Classes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        aggregate10BisMoneyToPoints();
+        Environment.Exit(0);
+    }
+
+    private static void aggregate10BisMoneyToPoints()
+    {
+        TenBisWebsite tenBisWebsite = null;
+
+        try
+        {
+            tenBisWebsite = new TenBisWebsite();
+            tenBisWebsite.StartTenBisWebsite();
+            tenBisWebsite.ValidateUserLoggedIn();
+            tenBisWebsite.AggregateMoneyToPoints();
+        }
+        finally
+        {
+            tenBisWebsite?.Dispose();
+        }
+    }
+}
