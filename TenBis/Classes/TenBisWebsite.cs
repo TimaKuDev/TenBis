0c4ee5a63474c2c5947314b356b09f429ca7398b
TimaKuDev <Timakudev@gmail.com>
2023-11-11 13:10:43 +0200

    added comments, and made small changes

--- TenBis/Classes/TenBisWebsite.cs
+++ TenBis/Classes/TenBisWebsite.cs
@@ -1,16 +1,15 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Text;
+using System.Collections.ObjectModel;
 
 namespace TenBis.Classes
 {
     internal class TenBisWebsite
     {
-        private const string TEN_BIS_URL = "https://www.10bis.co.il/next/user-report?dateBias=0";
         ChromeDriverService _chromeDriverService;
         ChromeOptions _chromeOptions;
         ChromeDriver _chromeDriver;
-        System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> _dropDownImage;
+        ReadOnlyCollection<IWebElement> _dropDownImage;
         public TenBisWebsite()
         {
             _chromeDriverService = ChromeDriverService.CreateDefaultService();
@@ -33,12 +32,14 @@ namespace TenBis.Classes
 
         public void StartTenBisWebsite()
         {
+            const string TEN_BIS_URL = "https://www.10bis.co.il/next/user-report?dateBias=0";
             _chromeDriver.Manage().Window.Maximize();
             _chromeDriver.Navigate().GoToUrl(TEN_BIS_URL);
         }
 
         public void ValidateUserLoggedIn()
         {
+            //Think about better name for the method
             Task.Delay(2000).Wait();
             const string DROP_DOWN_IMAGE = @"//img[@class=""styled__Img-sc-1f9rnwm-0 bDSlBz ActionMenu__MenuTriangleButton-sc-15ye1k-1 kwtrcZ""]";
             _dropDownImage = _chromeDriver.FindElements(By.XPath(DROP_DOWN_IMAGE));
@@ -53,14 +54,14 @@ namespace TenBis.Classes
         {
             Task.Delay(1000).Wait();
             const string DROP_DOWN_IMAGE = @"//button[@class=""Button-sc-1n9hyby-0 AddToCredit__AddCreditButton-sc-1ux4vzy-3 fQLuJl kipuFE""]";
-            System.Collections.ObjectModel.ReadOnlyCollection<IWebElement> aggregateButton = _chromeDriver.FindElements(By.XPath(DROP_DOWN_IMAGE));
+            ReadOnlyCollection<IWebElement> aggregateButton = _chromeDriver.FindElements(By.XPath(DROP_DOWN_IMAGE));
             if (!aggregateButton[0].Enabled)
             {
                 return;
             }
 
             aggregateButton[0].Click();
-           
+           //Continue code here
         }
     }
 }
