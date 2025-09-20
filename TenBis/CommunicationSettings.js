ccd7251ab066519d1ab68c329e32f57ec6a035f6
TimaKuDev <Timakudev@gmail.com>
2025-09-20 22:54:28 +0300

    add dummy settgs

--- TenBis/CommunicationSettings.js
+++ TenBis/CommunicationSettings.js
@@ -1,1 +1,14 @@
-﻿
+﻿{
+  "communication": "Telegram",
+  "email": {
+    "smtpServer": "smtp.gmail.com",
+    "port": 465,
+    "username": "gmail1@gmail.com",
+    "password": "1",
+    "recipientEmail": "gmail@gmail.co.il"
+  },
+  "telegram": {
+    "botToken": "1:1-2",
+    "chatId": 3
+  }
+}
